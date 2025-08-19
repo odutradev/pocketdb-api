0d7c5e48edb6f5fc48108031947697a180eb7ec8
odutradev <joao.vitornl@gmail.com>
2025-08-19 11:30:34 -0300

    fix: Correction in endpoints

--- src/routes/resources/kv.router.ts
+++ src/routes/resources/kv.router.ts
@@ -5,10 +5,10 @@ import kvResource from "@resources/kv/kv.resource";
 
 const kvRouter = Router();
 
-kvRouter.delete("/:collection/delete", manageRequest(kvResource.deleteCollection));
-kvRouter.patch("/:collection/update/:id", manageRequest(kvResource.updateById));
-kvRouter.delete("/project/delete", manageRequest(kvResource.deleteProject));
+kvRouter.delete("/:collection/delete-all", manageRequest(kvResource.deleteCollection));
 kvRouter.delete("/:collection/delete/:id", manageRequest(kvResource.deleteById));
+kvRouter.patch("/:collection/update/:id", manageRequest(kvResource.updateById));
+kvRouter.delete("/project/delete-all", manageRequest(kvResource.deleteProject));
 kvRouter.get("/:collection/get/:id", manageRequest(kvResource.getById));
 kvRouter.post("/:collection/create", manageRequest(kvResource.create));
 
